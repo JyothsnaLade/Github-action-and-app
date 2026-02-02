32472fda68cde2141822d83deb6bc987d1f100a4
jyothsnalade <jyothsnalade9@gmail.com>
2026-02-02 09:33:04 -0800

    testing

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def greet(name):
   """
   This function greets the person passed in as a parameter.
   """
-  print(f"Hello, {name}!")
+  print(f"Hello, {name}!") 
 
 if __name__ == "__main__":
   # Call the greet function with a sample name
